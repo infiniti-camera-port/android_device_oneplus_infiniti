947f88d006feededbccbd72b237dfb5450d7a086
1vivy <1vivy@tutanota.com>
2026-06-14 01:06:00 -0600

    device/oneplus/infiniti: wire in the Oplus camera
    Wire up Oplus camera support for infiniti/sm8850:
    - device.mk: inherit-product-if-exists vendor/oplus/camera/opluscamera.mk
    - BoardConfig.mk: BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
    - extract-files.py: drop the CameraHWConfiguration SW-face-detect-disable fixup
      (keep it ON for the Oplus camera); add the libsharebuffer_impl ->
      libutils-stock/libui-stock ABI workaround + libui-stock graphics.common
      V5->V7 (libsharebuffer_impl NEEDs libui/libutils; both present in the vendor blobs)
    - proprietary-files.txt: extract vendor libutils/libui as -stock (FIX_SONAME)
    
    (cherry picked from commit 7a56a1b385299ff9b582b2dd47aa4a6df346a70d)

--- extract-files.py
+++ extract-files.py
@@ -37,10 +37,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
-        # Disable face detection AE behaviour
-        .regex_replace(r'(enableSWfdForThirdCamUnit += )TRUE', r'\1FALSE')
-        .regex_replace(r'(fdSupport += )TRUE;', r'\1FALSE;'),
     'odm/etc/init/init.camera_process.rc': blob_fixup()
         .regex_replace('    delete_recursion', '    #delete_recursion'),
     (
@@ -84,6 +80,12 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libcamxdumpinforecorder.so',
     ): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v36.so'),
+    # Oplus camera: libsharebuffer_impl needs the stock libutils/libui ABI
+    'odm/lib64/libsharebuffer_impl.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-stock.so')
+        .replace_needed('libui.so', 'libui-stock.so'),
+    'vendor/lib64/libui-stock.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
